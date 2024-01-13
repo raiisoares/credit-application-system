2c767a00ccea7e662484569e0521d05dfb7e3673
raiisoares <raisoares.dev@gmail.com>
2024-01-12 22:19:23 -0300

    Added jpa and database configs.

--- src/main/kotlin/me/dio/credit/application/system/entities/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entities/Customer.kt
@@ -1,13 +1,33 @@
 package me.dio.credit.application.system.entities
 
+import jakarta.persistence.*
+
+@Entity
 data class Customer(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false)
     var firstName: String = "",
+
+    @Column(nullable = false)
     var lastName: String = "",
+
+    @Column(nullable = false, unique = true)
     val cpf: String,
+
+    @Column(nullable = false, unique = true)
     var email: String = "",
+
+    @Column(nullable = false)
     var password: String = "",
+
+    @Column(nullable = false)
+    @Embedded
     val address: Address = Address(),
-    var credits: List<Credit> = mutableListOf(),
-    val id: Long? = null
+
+    @Column(nullable = false)
+    @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE], mappedBy = "customer")
+    var credits: List<Credit> = mutableListOf()
 )
 
